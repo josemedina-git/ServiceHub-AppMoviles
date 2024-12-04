c9d9e95116d42f8588c9afcaecf99a0ef728aa21
josemedina-git <joseign03mr@gmail.com>
2024-12-04 07:25:27 -0600

    AddDetails

--- ServiceHub/app/src/main/java/com/example/servicehub/ProProfessional.java
+++ ServiceHub/app/src/main/java/com/example/servicehub/ProProfessional.java
@@ -3,6 +3,7 @@ package com.example.servicehub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +20,8 @@ public class ProProfessional extends AppCompatActivity {
 
         back = findViewById(R.id.backArrow_pro);
         reviews = findViewById(R.id.ibtn_reviews);
+        tel = findViewById(R.id.ibtn_tel);
+        share = findViewById(R.id.ibtn_share);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,6 +35,28 @@ public class ProProfessional extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(ProProfessional.this, Reviews.class);
+                startActivity(intent);
+            }
+        });
+
+        tel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:" + "4492768717"));
+                startActivity(intent);
+            }
+        });
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/plain");
+                intent.putExtra(Intent.EXTRA_SUBJECT, "asunto");
+                intent.putExtra(Intent.EXTRA_TEXT, "texto del correo");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[] {"joseign03mr@gmail.com"});
+
                 startActivity(intent);
             }
         });
